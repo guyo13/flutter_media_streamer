4aefe0f934b3e8d338412e0fcac74b6d569e6b26
Guy Or <guy@guyor.net>
2020-09-30 17:34:30 +0300

    correctly implement image metadata fetching and add method call for permissions

--- ios/Classes/SwiftFlutterMediaStreamerPlugin.swift
+++ ios/Classes/SwiftFlutterMediaStreamerPlugin.swift
@@ -21,49 +21,68 @@ public class SwiftFlutterMediaStreamerPlugin: NSObject, FlutterPlugin, FlutterAp
         self.imageFetchResult = nil
         self.encoder = JSONEncoder()
     }
-
+    
     public func handle( _ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        guard let args = call.arguments as? Dictionary<String, Any> else {
-            NSLog("Inavlid arguments passed via method channel")
-            result(FlutterError(code: "Invalid_Arguments", message: nil, details: nil))
-            return
-        }
         switch call.method {
         case "getPlatformVersion":
             result("iOS " + UIDevice.current.systemVersion)
         case "getThumbnail":
+            if !checkDictionaryArgs(call: call) {
+                result(FlutterError(code: "Invalid_Arguments", message: nil, details: nil))
+            }
+            let args = call.arguments as! Dictionary<String, Any>
             let w: Int = (args["width"] as? NSNumber)?.intValue ?? 640
             let h: Int = (args["height"] as? NSNumber)?.intValue ?? 400
             let contentUriString: String = args["height"] as! String
             getThumbnail(result: result, uriString: contentUriString, width: w, height: h)
         case "getImage":
+            if !checkDictionaryArgs(call: call) {
+                result(FlutterError(code: "Invalid_Arguments", message: nil, details: nil))
+            }
+            let args = call.arguments as! Dictionary<String, Any>
             let w: Int? = (args["width"] as? NSNumber)?.intValue
             let h: Int? = (args["height"] as? NSNumber)?.intValue
             let contentUriString: String = args["height"] as! String
             getImage(result: result, uriString: contentUriString, width: w, height: h)
         case "streamGalleryImages":
+            if !checkDictionaryArgs(call: call) {
+                result(FlutterError(code: "Invalid_Arguments", message: nil, details: nil))
+            }
+            let args = call.arguments as! Dictionary<String, Any>
             let columns = args["columns"] as! [String]
             let limit = (args["limit"] as? NSNumber)?.intValue ?? 10
             let offset = (args["offset"] as? NSNumber)?.intValue ?? 0
             streamGalleryImages(result: result, columns: columns, limit: limit, offset: offset)
         case "haveStoragePermission":
             result(havePermissions())
+        case "requestStoragePermissions":
+            requestPermissionsAndRegister(handler: {authResults in result(authResults == .authorized)})
         default:
             result(FlutterMethodNotImplemented)
         }
     }
-
+    
+    private func checkDictionaryArgs(call: FlutterMethodCall) -> Bool {
+        guard (call.arguments as? Dictionary<String, Any>) != nil else {
+            NSLog("Inavlid arguments passed via method channel")
+            return false
+        }
+        return true
+    }
+    
     private func getThumbnail(result: @escaping FlutterResult, uriString: String, width:Int = 640, height:Int = 400) {
-
+        
     }
     private func getImage(result: @escaping FlutterResult, uriString: String, width:Int?, height:Int?) {
-
+        
     }
     
     private func streamGalleryImages(result: @escaping FlutterResult, columns: [String], limit: Int = 10, offset: Int = 0) {
         //If cursor already available, continue serving results through it
         if self.imageFetchResult != nil {
-            
+            self.queryQueue.async {
+                self.resumeImageCursor(result: result, cursor: self.imageFetchResult!, limit: limit, offset: offset)
+            }
         } else {
             // Check permissions on userInitiated queue
             self.permissionsQueue.async {
@@ -73,7 +92,10 @@ public class SwiftFlutterMediaStreamerPlugin: NSObject, FlutterPlugin, FlutterAp
                     self.requestPermissionsAndRegister { authStatus in
                         if (authStatus == PHAuthorizationStatus.authorized) {
                             // Execute query only if permissions were authorized
-                            self.getGalleryImageCursor(columns: columns, limit: limit, offset: offset)
+                            self.queryQueue.async {
+                                self.getGalleryImageCursor(columns: columns, limit: limit, offset: offset)
+                                self.resumeImageCursor(result: result, cursor: self.imageFetchResult!, limit: limit, offset: offset)
+                            }
                         } else {
                             result(FlutterError(code: "ERR_PERMISSIONS",
                                                 message: "Photo Gallery Permissions denied", details: nil))
@@ -81,48 +103,51 @@ public class SwiftFlutterMediaStreamerPlugin: NSObject, FlutterPlugin, FlutterAp
                     }
                 } else {
                     // Permissions available continue execution (on utility thread)
-                    self.getGalleryImageCursor(columns: columns, limit: limit, offset: offset)
-                }
-            }
-        }
-    }
-    
-    private func getGalleryImageCursor(columns: [String], limit: Int = 10, offset: Int = 0) {
-        self.queryQueue.async {
-            let fetchOptions = PHFetchOptions()
-            let sources: PHAssetSourceType = [.typeUserLibrary, .typeCloudShared, .typeiTunesSynced]
-            fetchOptions.includeAssetSourceTypes = sources
-            let fetchResult: PHFetchResult<PHAsset> = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: fetchOptions)
-            DispatchQueue.main.sync {
-                self.imageFetchResult = fetchResult
-            }
-        }
-    }
-    
-    private func resumeImageCursor(result: @escaping FlutterResult, cursor: PHFetchResult<PHAsset>, limit: Int = 10, offset: Int = 0) {
-        self.queryQueue.async {
-            let start = offset
-            let desiredEnd = offset + limit - 1
-            let end = desiredEnd < cursor.count ? desiredEnd : (cursor.count - 1)
-            let indexSet = IndexSet(start...end)
-            let objects = cursor.objects(at: indexSet)
-            var res = [String]()
-            for item in objects {
-                //TODO - serialize to json and add to res
-                do {
-                    let data = try self.encoder.encode(EncodableAsset.init(with: item))
-                    let jsonString = String(data: data, encoding: .utf8)
-                    if (jsonString != nil) {
-                        res.append(jsonString!)
+                    self.queryQueue.async {
+                        self.getGalleryImageCursor(columns: columns, limit: limit, offset: offset)
+                        self.resumeImageCursor(result: result, cursor: self.imageFetchResult!, limit: limit, offset: offset)
                     }
                 }
-                catch {
-                    print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // Run only from worker thread
+    private func getGalleryImageCursor(columns: [String], limit: Int = 10, offset: Int = 0) {
+        let fetchOptions = PHFetchOptions()
+        let sources: PHAssetSourceType = [.typeUserLibrary, .typeCloudShared, .typeiTunesSynced]
+        fetchOptions.includeAssetSourceTypes = sources
+        let fetchResult: PHFetchResult<PHAsset> = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: fetchOptions)
+        DispatchQueue.main.sync {
+            self.imageFetchResult = fetchResult
+        }
+        
+    }
+    
+    // Run only from worker thread
+    private func resumeImageCursor(result: @escaping FlutterResult, cursor: PHFetchResult<PHAsset>, limit: Int = 10, offset: Int = 0) {
+        let start = offset
+        let desiredEnd = offset + limit - 1
+        let end = desiredEnd < cursor.count ? desiredEnd : (cursor.count - 1)
+        let indexSet = IndexSet(start...end)
+        let objects = cursor.objects(at: indexSet)
+        var res = [String]()
+        for item in objects {
+            //TODO - serialize to json and add to res
+            do {
+                let data = try self.encoder.encode(EncodableAsset.init(with: item))
+                let jsonString = String(data: data, encoding: .utf8)
+                if (jsonString != nil) {
+                    res.append(jsonString!)
                 }
             }
-            print(res)
-            result(res)
+            catch {
+                print(error.localizedDescription)
+            }
         }
+        print(res)
+        result(res)
+        
     }
     
     private func havePermissions() -> Bool {
@@ -138,7 +163,7 @@ public class SwiftFlutterMediaStreamerPlugin: NSObject, FlutterPlugin, FlutterAp
     private func requestPermissionsAndRegister(handler: @escaping (PHAuthorizationStatus) -> Void) {
         PHPhotoLibrary.requestAuthorization(handler)
     }
-
+    
     public func detachFromEngine(for registrar: FlutterPluginRegistrar) {
         //TODO
     }
